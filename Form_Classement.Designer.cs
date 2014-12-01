9b372d20583c40a19ccfd5b4b2c1dd41b552bc39
manoslive <ebeloin@hotmail.com>
2014-12-01 13:48:21 -0500

    Form Classement setup

--- Form_Classement.Designer.cs
+++ Form_Classement.Designer.cs
@@ -28,34 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.DGV_Classement = new System.Windows.Forms.DataGridView();
+            this.BTN_Fermer = new System.Windows.Forms.Button();
+            this.LB_Division = new System.Windows.Forms.Label();
+            this.CB_Division = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Classement)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // DGV_Classement
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(603, 398);
-            this.dataGridView1.TabIndex = 0;
+            this.DGV_Classement.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_Classement.Location = new System.Drawing.Point(22, 24);
+            this.DGV_Classement.MultiSelect = false;
+            this.DGV_Classement.Name = "DGV_Classement";
+            this.DGV_Classement.ReadOnly = true;
+            this.DGV_Classement.RowHeadersVisible = false;
+            this.DGV_Classement.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.DGV_Classement.RowTemplate.Height = 28;
+            this.DGV_Classement.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DGV_Classement.Size = new System.Drawing.Size(586, 350);
+            this.DGV_Classement.TabIndex = 1;
+            // 
+            // BTN_Fermer
+            // 
+            this.BTN_Fermer.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BTN_Fermer.Location = new System.Drawing.Point(506, 400);
+            this.BTN_Fermer.Name = "BTN_Fermer";
+            this.BTN_Fermer.Size = new System.Drawing.Size(102, 33);
+            this.BTN_Fermer.TabIndex = 2;
+            this.BTN_Fermer.Text = "Fermer";
+            this.BTN_Fermer.UseVisualStyleBackColor = true;
+            // 
+            // LB_Division
+            // 
+            this.LB_Division.AutoSize = true;
+            this.LB_Division.Location = new System.Drawing.Point(325, 386);
+            this.LB_Division.Name = "LB_Division";
+            this.LB_Division.Size = new System.Drawing.Size(63, 20);
+            this.LB_Division.TabIndex = 6;
+            this.LB_Division.Text = "Division";
+            // 
+            // CB_Division
+            // 
+            this.CB_Division.FormattingEnabled = true;
+            this.CB_Division.Items.AddRange(new object[] {
+            "Est / Ouest"});
+            this.CB_Division.Location = new System.Drawing.Point(329, 408);
+            this.CB_Division.Name = "CB_Division";
+            this.CB_Division.Size = new System.Drawing.Size(152, 28);
+            this.CB_Division.TabIndex = 5;
             // 
             // Form_Classement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 494);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(629, 447);
+            this.Controls.Add(this.LB_Division);
+            this.Controls.Add(this.CB_Division);
+            this.Controls.Add(this.BTN_Fermer);
+            this.Controls.Add(this.DGV_Classement);
             this.Name = "Form_Classement";
             this.Text = "Form_Classement";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Form_Classement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_Classement)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView DGV_Classement;
+        private System.Windows.Forms.Button BTN_Fermer;
+        private System.Windows.Forms.Label LB_Division;
+        public System.Windows.Forms.ComboBox CB_Division;
+
     }
 }
--- Form_Classement.cs
+++ Form_Classement.cs
@@ -5,16 +5,72 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.DataAccess.Client;
 
 namespace TPFinal
 {
     public partial class Form_Classement : Form
     {
-        public Form_Classement()
+        private OracleConnection oracon = null;
+        private MaConnection connection = null;
+        private DataSet dataSetClassement = new DataSet();
+        public Form callBackForm = null;
+        public Form_Classement(OracleConnection laConnection, MaConnection maBelleConnection)
         {
             InitializeComponent();
+            oracon = laConnection;
+            connection = maBelleConnection;
+        }
+        private void LoadDGV()
+        {
+            string classementDGV = null;
+            DGV_Classement.AllowUserToResizeColumns = false; // Empêche le resize des colonnes
+            DGV_Classement.AllowUserToResizeRows = false; // Empêche le resize des rangées
+            DGV_Classement.AllowUserToAddRows = false; // Enlève la ligne vide à la fin du DGV
+            dataSetClassement.Clear(); // Vide le dataset afin de ne pas avoir de doublons
+            int lastIndex = -1;
+            if (DGV_Classement.SelectedRows.Count > 0)
+                lastIndex = DGV_Classement.SelectedRows[0].Index;
+            OracleCommand oraSelect = oracon.CreateCommand();
+            OracleDataAdapter oraAdapter = new OracleDataAdapter(oraSelect);
+
+            classementDGV = DGV_Classement.Text;
+            if (classementDGV != "Est" && classementDGV != "Ouest")
+                classementDGV = "Est' or e.nomdivision='Ouest'";
+            else
+                classementDGV += "'";
+            oraSelect.CommandText = "SELECT NomEquipe, DateIntro as DateIntroLigue, d.NOMDIVISION, Ville FROM Equipe E " +
+                                    "inner join Division D on D.NomDivision = E.NOMDIVISION " +
+                                    "WHERE e.nomDivision = '" + classementDGV;
+            oraAdapter.Fill(dataSetClassement, "tableFormEquipe");
+            DGV_Classement.DataSource = dataSetClassement.Tables[0];
+            // SetDGVLargeurColonne();
+            if (lastIndex > -1 && DGV_Classement.Rows.Count > 0)
+                DGV_Classement.Rows[Math.Min(lastIndex, DGV_Classement.Rows.Count - 1)].Selected = true;
+        }
+
+        private void Form_Classement_Load(object sender, EventArgs e)
+        {
+            LoadDGV();
+            LoadCBDivision();
+        }
+
+        private void LoadCBDivision()
+        {
+            OracleCommand oraSelect = oracon.CreateCommand();
+            oraSelect.CommandText = "SELECT NomDivision FROM division";
+            using (OracleDataReader oraReader = oraSelect.ExecuteReader())
+            {
+                while (oraReader.Read())
+                {
+                    CB_Division.Items.Add(oraReader.GetString(0));
+                }
+                oraReader.Close();
+            }
+            CB_Division.SelectedIndex = 0;
         }
     }
 }
