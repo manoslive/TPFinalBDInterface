d227f94f9b822f4e7f5c9b3cf74059a623134a41
Shaun Cooper <shaun.11.cooper@gmail.com>
2014-12-02 16:26:17 -0500

    sad

--- Form_Top5.Designer.cs
+++ Form_Top5.Designer.cs
@@ -49,40 +49,36 @@
             // DGV_Top3
             // 
             this.DGV_Top3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Top3.Location = new System.Drawing.Point(301, 334);
-            this.DGV_Top3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DGV_Top3.Location = new System.Drawing.Point(201, 217);
             this.DGV_Top3.Name = "DGV_Top3";
-            this.DGV_Top3.Size = new System.Drawing.Size(15, 15);
+            this.DGV_Top3.Size = new System.Drawing.Size(10, 10);
             this.DGV_Top3.TabIndex = 4;
             this.DGV_Top3.Visible = false;
             // 
             // LB_Top1
             // 
             this.LB_Top1.AutoSize = true;
-            this.LB_Top1.Location = new System.Drawing.Point(297, 18);
-            this.LB_Top1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Top1.Location = new System.Drawing.Point(198, 12);
             this.LB_Top1.Name = "LB_Top1";
-            this.LB_Top1.Size = new System.Drawing.Size(51, 20);
+            this.LB_Top1.Size = new System.Drawing.Size(35, 13);
             this.LB_Top1.TabIndex = 5;
             this.LB_Top1.Text = "label1";
             // 
             // LB_Top2
             // 
             this.LB_Top2.AutoSize = true;
-            this.LB_Top2.Location = new System.Drawing.Point(72, 83);
-            this.LB_Top2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Top2.Location = new System.Drawing.Point(48, 54);
             this.LB_Top2.Name = "LB_Top2";
-            this.LB_Top2.Size = new System.Drawing.Size(51, 20);
+            this.LB_Top2.Size = new System.Drawing.Size(35, 13);
             this.LB_Top2.TabIndex = 6;
             this.LB_Top2.Text = "label2";
             // 
             // LB_Top3
             // 
             this.LB_Top3.AutoSize = true;
-            this.LB_Top3.Location = new System.Drawing.Point(516, 126);
-            this.LB_Top3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LB_Top3.Location = new System.Drawing.Point(344, 82);
             this.LB_Top3.Name = "LB_Top3";
-            this.LB_Top3.Size = new System.Drawing.Size(51, 20);
+            this.LB_Top3.Size = new System.Drawing.Size(35, 13);
             this.LB_Top3.TabIndex = 7;
             this.LB_Top3.Text = "label3";
             // 
@@ -91,40 +87,36 @@
             this.PB_Star.BackColor = System.Drawing.Color.Transparent;
             this.PB_Star.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.PB_Star.Image = global::TPFinal.Properties.Resources.Star;
-            this.PB_Star.Location = new System.Drawing.Point(257, 294);
-            this.PB_Star.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PB_Star.Location = new System.Drawing.Point(171, 191);
             this.PB_Star.Name = "PB_Star";
-            this.PB_Star.Size = new System.Drawing.Size(126, 112);
+            this.PB_Star.Size = new System.Drawing.Size(84, 73);
             this.PB_Star.TabIndex = 8;
             this.PB_Star.TabStop = false;
             // 
             // PB_Top2
             // 
             this.PB_Top2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PB_Top2.Location = new System.Drawing.Point(13, 107);
-            this.PB_Top2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PB_Top2.Location = new System.Drawing.Point(9, 70);
             this.PB_Top2.Name = "PB_Top2";
-            this.PB_Top2.Size = new System.Drawing.Size(176, 134);
+            this.PB_Top2.Size = new System.Drawing.Size(117, 87);
             this.PB_Top2.TabIndex = 3;
             this.PB_Top2.TabStop = false;
             // 
             // PB_Top3
             // 
             this.PB_Top3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PB_Top3.Location = new System.Drawing.Point(450, 150);
-            this.PB_Top3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PB_Top3.Location = new System.Drawing.Point(300, 97);
             this.PB_Top3.Name = "PB_Top3";
-            this.PB_Top3.Size = new System.Drawing.Size(176, 134);
+            this.PB_Top3.Size = new System.Drawing.Size(117, 87);
             this.PB_Top3.TabIndex = 2;
             this.PB_Top3.TabStop = false;
             // 
             // PB_Top1
             // 
             this.PB_Top1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.PB_Top1.Location = new System.Drawing.Point(231, 43);
-            this.PB_Top1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PB_Top1.Location = new System.Drawing.Point(154, 28);
             this.PB_Top1.Name = "PB_Top1";
-            this.PB_Top1.Size = new System.Drawing.Size(176, 134);
+            this.PB_Top1.Size = new System.Drawing.Size(117, 87);
             this.PB_Top1.TabIndex = 1;
             this.PB_Top1.TabStop = false;
             // 
@@ -133,28 +125,28 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::TPFinal.Properties.Resources.Podium;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 186);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 121);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(612, 220);
+            this.pictureBox1.Size = new System.Drawing.Size(408, 143);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // BTN_Fermer
             // 
             this.BTN_Fermer.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.BTN_Fermer.Location = new System.Drawing.Point(272, 418);
+            this.BTN_Fermer.Location = new System.Drawing.Point(181, 272);
+            this.BTN_Fermer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BTN_Fermer.Name = "BTN_Fermer";
-            this.BTN_Fermer.Size = new System.Drawing.Size(95, 34);
+            this.BTN_Fermer.Size = new System.Drawing.Size(63, 22);
             this.BTN_Fermer.TabIndex = 9;
             this.BTN_Fermer.Text = "Fermer";
             this.BTN_Fermer.UseVisualStyleBackColor = true;
             // 
             // Form_Top5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 464);
+            this.ClientSize = new System.Drawing.Size(426, 302);
             this.Controls.Add(this.BTN_Fermer);
             this.Controls.Add(this.PB_Star);
             this.Controls.Add(this.LB_Top3);
@@ -166,7 +158,6 @@
             this.Controls.Add(this.PB_Top1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_Top5";
             this.Text = "Form_Top5";
             this.Load += new System.EventHandler(this.Form_Top5_Load);
