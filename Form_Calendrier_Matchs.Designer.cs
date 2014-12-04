9b171500649c377de937ce2d29b1f4bfd9d63b6c
Shaun Cooper <shaun.11.cooper@gmail.com>
2014-12-04 18:46:36 -0500

    BTN fermer form match

--- Form_Calendrier_Matchs.Designer.cs
+++ Form_Calendrier_Matchs.Designer.cs
@@ -32,15 +32,18 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.DGV_CalendierMatch = new System.Windows.Forms.DataGridView();
-            this.BTN_Fermer = new System.Windows.Forms.Button();
+            this.PB_Fermer_Gif = new System.Windows.Forms.PictureBox();
+            this.FB_Fermer = new FlashButton.FlashButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_CalendierMatch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fermer_Gif)).BeginInit();
             this.SuspendLayout();
             // 
             // Calendrier_Match
             // 
-            this.Calendrier_Match.Location = new System.Drawing.Point(371, 12);
+            this.Calendrier_Match.Location = new System.Drawing.Point(247, 8);
+            this.Calendrier_Match.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Calendrier_Match.Name = "Calendrier_Match";
             this.Calendrier_Match.TabIndex = 0;
             this.Calendrier_Match.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.Calendrier_Match_DateChanged);
@@ -48,9 +51,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::TPFinal.Properties.Resources.canada_hockey;
-            this.pictureBox2.Location = new System.Drawing.Point(83, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(55, 8);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(213, 252);
+            this.pictureBox2.Size = new System.Drawing.Size(142, 164);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -58,9 +62,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::TPFinal.Properties.Resources.Hockey_croise;
-            this.pictureBox1.Location = new System.Drawing.Point(763, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(509, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(213, 252);
+            this.pictureBox1.Size = new System.Drawing.Size(142, 164);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -68,7 +73,8 @@
             // DGV_CalendierMatch
             // 
             this.DGV_CalendierMatch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_CalendierMatch.Location = new System.Drawing.Point(12, 270);
+            this.DGV_CalendierMatch.Location = new System.Drawing.Point(8, 175);
+            this.DGV_CalendierMatch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DGV_CalendierMatch.MultiSelect = false;
             this.DGV_CalendierMatch.Name = "DGV_CalendierMatch";
             this.DGV_CalendierMatch.ReadOnly = true;
@@ -76,30 +82,49 @@
             this.DGV_CalendierMatch.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.DGV_CalendierMatch.RowTemplate.Height = 28;
             this.DGV_CalendierMatch.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGV_CalendierMatch.Size = new System.Drawing.Size(1000, 239);
+            this.DGV_CalendierMatch.Size = new System.Drawing.Size(667, 155);
             this.DGV_CalendierMatch.TabIndex = 6;
             // 
-            // BTN_Fermer
+            // PB_Fermer_Gif
             // 
-            this.BTN_Fermer.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.BTN_Fermer.Location = new System.Drawing.Point(910, 532);
-            this.BTN_Fermer.Name = "BTN_Fermer";
-            this.BTN_Fermer.Size = new System.Drawing.Size(102, 36);
-            this.BTN_Fermer.TabIndex = 7;
-            this.BTN_Fermer.Text = "Fermer";
-            this.BTN_Fermer.UseVisualStyleBackColor = true;
+            this.PB_Fermer_Gif.Image = global::TPFinal.Properties.Resources.ICON_Fermer_Over;
+            this.PB_Fermer_Gif.Location = new System.Drawing.Point(631, 335);
+            this.PB_Fermer_Gif.Name = "PB_Fermer_Gif";
+            this.PB_Fermer_Gif.Size = new System.Drawing.Size(40, 40);
+            this.PB_Fermer_Gif.TabIndex = 17;
+            this.PB_Fermer_Gif.TabStop = false;
+            this.PB_Fermer_Gif.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PB_Fermer_Gif_MouseDown);
+            this.PB_Fermer_Gif.MouseLeave += new System.EventHandler(this.PB_Fermer_Gif_MouseLeave);
+            this.PB_Fermer_Gif.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PB_Fermer_Gif_MouseUp);
+            // 
+            // FB_Fermer
+            // 
+            this.FB_Fermer.BackColor = System.Drawing.Color.Transparent;
+            this.FB_Fermer.BackgroundImage = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FB_Fermer.ImageClick = global::TPFinal.Properties.Resources.ICON_Fermer_Click;
+            this.FB_Fermer.ImageDisable = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.ImageNeutral = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.ImageOver = null;
+            this.FB_Fermer.Location = new System.Drawing.Point(631, 335);
+            this.FB_Fermer.Name = "FB_Fermer";
+            this.FB_Fermer.Size = new System.Drawing.Size(40, 40);
+            this.FB_Fermer.TabIndex = 16;
+            this.FB_Fermer.MouseEnter += new System.EventHandler(this.FB_Fermer_MouseEnter);
             // 
             // Form_Calendrier_Matchs
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1024, 580);
-            this.Controls.Add(this.BTN_Fermer);
+            this.ClientSize = new System.Drawing.Size(683, 377);
+            this.Controls.Add(this.PB_Fermer_Gif);
+            this.Controls.Add(this.FB_Fermer);
             this.Controls.Add(this.DGV_CalendierMatch);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Calendrier_Match);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_Calendrier_Matchs";
             this.Text = "Form_Calendrier_Match";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Calendrier_Matchs_FormClosing);
@@ -107,6 +132,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_CalendierMatch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fermer_Gif)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -117,6 +143,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.DataGridView DGV_CalendierMatch;
-        private System.Windows.Forms.Button BTN_Fermer;
+        private System.Windows.Forms.PictureBox PB_Fermer_Gif;
+        private FlashButton.FlashButton FB_Fermer;
     }
 }
