be64e0125377f95372a966139ec506e16fad0d5e
Shaun Cooper <shaun.11.cooper@gmail.com>
2014-12-04 18:29:45 -0500

    Form Classement Fermer btn

--- Form_Classement.Designer.cs
+++ Form_Classement.Designer.cs
@@ -29,16 +29,19 @@
         private void InitializeComponent()
         {
             this.DGV_Classement = new System.Windows.Forms.DataGridView();
-            this.BTN_Fermer = new System.Windows.Forms.Button();
             this.LB_Division = new System.Windows.Forms.Label();
             this.CB_Division = new System.Windows.Forms.ComboBox();
+            this.FB_Fermer = new FlashButton.FlashButton();
+            this.PB_Fermer_Gif = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Classement)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fermer_Gif)).BeginInit();
             this.SuspendLayout();
             // 
             // DGV_Classement
             // 
             this.DGV_Classement.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DGV_Classement.Location = new System.Drawing.Point(22, 24);
+            this.DGV_Classement.Location = new System.Drawing.Point(15, 16);
+            this.DGV_Classement.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DGV_Classement.MultiSelect = false;
             this.DGV_Classement.Name = "DGV_Classement";
             this.DGV_Classement.ReadOnly = true;
@@ -46,25 +49,16 @@
             this.DGV_Classement.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.DGV_Classement.RowTemplate.Height = 28;
             this.DGV_Classement.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DGV_Classement.Size = new System.Drawing.Size(586, 350);
+            this.DGV_Classement.Size = new System.Drawing.Size(391, 227);
             this.DGV_Classement.TabIndex = 1;
-            // 
-            // BTN_Fermer
-            // 
-            this.BTN_Fermer.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.BTN_Fermer.Location = new System.Drawing.Point(506, 400);
-            this.BTN_Fermer.Name = "BTN_Fermer";
-            this.BTN_Fermer.Size = new System.Drawing.Size(102, 33);
-            this.BTN_Fermer.TabIndex = 2;
-            this.BTN_Fermer.Text = "Fermer";
-            this.BTN_Fermer.UseVisualStyleBackColor = true;
             // 
             // LB_Division
             // 
             this.LB_Division.AutoSize = true;
-            this.LB_Division.Location = new System.Drawing.Point(325, 386);
+            this.LB_Division.Location = new System.Drawing.Point(217, 251);
+            this.LB_Division.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LB_Division.Name = "LB_Division";
-            this.LB_Division.Size = new System.Drawing.Size(63, 20);
+            this.LB_Division.Size = new System.Drawing.Size(44, 13);
             this.LB_Division.TabIndex = 6;
             this.LB_Division.Text = "Division";
             // 
@@ -73,27 +67,58 @@
             this.CB_Division.FormattingEnabled = true;
             this.CB_Division.Items.AddRange(new object[] {
             "Toutes"});
-            this.CB_Division.Location = new System.Drawing.Point(329, 408);
+            this.CB_Division.Location = new System.Drawing.Point(219, 265);
+            this.CB_Division.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CB_Division.Name = "CB_Division";
-            this.CB_Division.Size = new System.Drawing.Size(152, 28);
+            this.CB_Division.Size = new System.Drawing.Size(103, 21);
             this.CB_Division.TabIndex = 5;
             this.CB_Division.SelectedIndexChanged += new System.EventHandler(this.CB_Division_SelectedIndexChanged);
             // 
+            // FB_Fermer
+            // 
+            this.FB_Fermer.BackColor = System.Drawing.Color.Transparent;
+            this.FB_Fermer.BackgroundImage = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FB_Fermer.ImageClick = global::TPFinal.Properties.Resources.ICON_Fermer_Click;
+            this.FB_Fermer.ImageDisable = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.ImageNeutral = global::TPFinal.Properties.Resources.ICON_Fermer;
+            this.FB_Fermer.ImageOver = null;
+            this.FB_Fermer.Location = new System.Drawing.Point(366, 250);
+            this.FB_Fermer.Name = "FB_Fermer";
+            this.FB_Fermer.Size = new System.Drawing.Size(40, 40);
+            this.FB_Fermer.TabIndex = 7;
+            this.FB_Fermer.MouseEnter += new System.EventHandler(this.FB_Fermer_MouseEnter);
+            // 
+            // PB_Fermer_Gif
+            // 
+            this.PB_Fermer_Gif.Image = global::TPFinal.Properties.Resources.ICON_Fermer_Over;
+            this.PB_Fermer_Gif.Location = new System.Drawing.Point(366, 248);
+            this.PB_Fermer_Gif.Name = "PB_Fermer_Gif";
+            this.PB_Fermer_Gif.Size = new System.Drawing.Size(40, 40);
+            this.PB_Fermer_Gif.TabIndex = 8;
+            this.PB_Fermer_Gif.TabStop = false;
+            this.PB_Fermer_Gif.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PB_Fermer_Gif_MouseDown);
+            this.PB_Fermer_Gif.MouseLeave += new System.EventHandler(this.PB_Fermer_Gif_MouseLeave);
+            this.PB_Fermer_Gif.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PB_Fermer_Gif_MouseUp);
+            // 
             // Form_Classement
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 447);
+            this.ClientSize = new System.Drawing.Size(419, 291);
+            this.Controls.Add(this.PB_Fermer_Gif);
+            this.Controls.Add(this.FB_Fermer);
             this.Controls.Add(this.LB_Division);
             this.Controls.Add(this.CB_Division);
-            this.Controls.Add(this.BTN_Fermer);
             this.Controls.Add(this.DGV_Classement);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form_Classement";
             this.Text = "Form_Classement";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Classement_FormClosing);
             this.Load += new System.EventHandler(this.Form_Classement_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGV_Classement)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Fermer_Gif)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,9 +127,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView DGV_Classement;
-        private System.Windows.Forms.Button BTN_Fermer;
         private System.Windows.Forms.Label LB_Division;
         public System.Windows.Forms.ComboBox CB_Division;
+        private FlashButton.FlashButton FB_Fermer;
+        private System.Windows.Forms.PictureBox PB_Fermer_Gif;
 
     }
 }
