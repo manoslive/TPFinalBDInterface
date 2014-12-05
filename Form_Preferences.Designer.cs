12e97d86e7ebe8deac6c5cafcfd1cf5b4f00ed1b
Shaun Cooper <shaun.11.cooper@gmail.com>
2014-12-05 17:48:01 -0500

    Commencement preferences

--- Form_Preferences.Designer.cs
+++ Form_Preferences.Designer.cs
@@ -28,65 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.fb_Police = new FlashButton.FlashButton();
-            this.fb_LignesPaires = new FlashButton.FlashButton();
-            this.fb_LignesImpaires = new FlashButton.FlashButton();
+            this.FB_CouleurPoliceForm = new FlashButton.FlashButton();
+            this.FB_CouleurForm = new FlashButton.FlashButton();
+            this.FB_PoliceForm = new FlashButton.FlashButton();
             this.SuspendLayout();
             // 
-            // fb_Police
+            // FB_CouleurPoliceForm
             // 
-            this.fb_Police.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.fb_Police.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.fb_Police.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.fb_Police.ImageClick = null;
-            this.fb_Police.ImageDisable = null;
-            this.fb_Police.ImageNeutral = null;
-            this.fb_Police.ImageOver = null;
-            this.fb_Police.Location = new System.Drawing.Point(226, 14);
-            this.fb_Police.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.fb_Police.Name = "fb_Police";
-            this.fb_Police.Size = new System.Drawing.Size(89, 87);
-            this.fb_Police.TabIndex = 5;
+            this.FB_CouleurPoliceForm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FB_CouleurPoliceForm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.FB_CouleurPoliceForm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FB_CouleurPoliceForm.ImageClick = null;
+            this.FB_CouleurPoliceForm.ImageDisable = null;
+            this.FB_CouleurPoliceForm.ImageNeutral = null;
+            this.FB_CouleurPoliceForm.ImageOver = null;
+            this.FB_CouleurPoliceForm.Location = new System.Drawing.Point(119, 14);
+            this.FB_CouleurPoliceForm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FB_CouleurPoliceForm.Name = "FB_CouleurPoliceForm";
+            this.FB_CouleurPoliceForm.Size = new System.Drawing.Size(89, 87);
+            this.FB_CouleurPoliceForm.TabIndex = 3;
             // 
-            // fb_LignesPaires
+            // FB_CouleurForm
             // 
-            this.fb_LignesPaires.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.fb_LignesPaires.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.fb_LignesPaires.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.fb_LignesPaires.ImageClick = null;
-            this.fb_LignesPaires.ImageDisable = null;
-            this.fb_LignesPaires.ImageNeutral = null;
-            this.fb_LignesPaires.ImageOver = null;
-            this.fb_LignesPaires.Location = new System.Drawing.Point(119, 14);
-            this.fb_LignesPaires.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.fb_LignesPaires.Name = "fb_LignesPaires";
-            this.fb_LignesPaires.Size = new System.Drawing.Size(89, 87);
-            this.fb_LignesPaires.TabIndex = 3;
+            this.FB_CouleurForm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FB_CouleurForm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.FB_CouleurForm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FB_CouleurForm.ImageClick = null;
+            this.FB_CouleurForm.ImageDisable = null;
+            this.FB_CouleurForm.ImageNeutral = null;
+            this.FB_CouleurForm.ImageOver = null;
+            this.FB_CouleurForm.Location = new System.Drawing.Point(226, 14);
+            this.FB_CouleurForm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FB_CouleurForm.Name = "FB_CouleurForm";
+            this.FB_CouleurForm.Size = new System.Drawing.Size(89, 87);
+            this.FB_CouleurForm.TabIndex = 5;
             // 
-            // fb_LignesImpaires
+            // FB_PoliceForm
             // 
-            this.fb_LignesImpaires.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.fb_LignesImpaires.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.fb_LignesImpaires.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.fb_LignesImpaires.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.fb_LignesImpaires.ImageClick = null;
-            this.fb_LignesImpaires.ImageDisable = null;
-            this.fb_LignesImpaires.ImageNeutral = null;
-            this.fb_LignesImpaires.ImageOver = null;
-            this.fb_LignesImpaires.Location = new System.Drawing.Point(13, 14);
-            this.fb_LignesImpaires.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.fb_LignesImpaires.Name = "fb_LignesImpaires";
-            this.fb_LignesImpaires.Size = new System.Drawing.Size(89, 87);
-            this.fb_LignesImpaires.TabIndex = 4;
+            this.FB_PoliceForm.BackColor = System.Drawing.Color.Transparent;
+            this.FB_PoliceForm.BackgroundImage = global::TPFinal.Properties.Resources.ModifierPolice;
+            this.FB_PoliceForm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.FB_PoliceForm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.FB_PoliceForm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FB_PoliceForm.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FB_PoliceForm.ImageClick = global::TPFinal.Properties.Resources.ModifierPolice_Click;
+            this.FB_PoliceForm.ImageDisable = global::TPFinal.Properties.Resources.ModifierPolice;
+            this.FB_PoliceForm.ImageNeutral = global::TPFinal.Properties.Resources.ModifierPolice;
+            this.FB_PoliceForm.ImageOver = global::TPFinal.Properties.Resources.ModifierPolice_Over;
+            this.FB_PoliceForm.Location = new System.Drawing.Point(13, 14);
+            this.FB_PoliceForm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.FB_PoliceForm.Name = "FB_PoliceForm";
+            this.FB_PoliceForm.Size = new System.Drawing.Size(89, 87);
+            this.FB_PoliceForm.TabIndex = 4;
             // 
             // Form_Preferences
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(333, 121);
-            this.Controls.Add(this.fb_Police);
-            this.Controls.Add(this.fb_LignesPaires);
-            this.Controls.Add(this.fb_LignesImpaires);
+            this.Controls.Add(this.FB_CouleurForm);
+            this.Controls.Add(this.FB_CouleurPoliceForm);
+            this.Controls.Add(this.FB_PoliceForm);
             this.Name = "Form_Preferences";
             this.Text = "Préférences";
             this.ResumeLayout(false);
@@ -95,8 +97,8 @@
 
         #endregion
 
-        private FlashButton.FlashButton fb_Police;
-        private FlashButton.FlashButton fb_LignesPaires;
-        private FlashButton.FlashButton fb_LignesImpaires;
+        private FlashButton.FlashButton FB_CouleurPoliceForm;
+        private FlashButton.FlashButton FB_PoliceForm;
+        private FlashButton.FlashButton FB_CouleurForm;
     }
 }
